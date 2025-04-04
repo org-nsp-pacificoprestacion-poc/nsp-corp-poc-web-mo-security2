323d346226cc75149f197291f4ed25341aa441d3
Diego CM <diezer.diego@gmail.com>
2025-04-04 15:02:29 -0500

    feat:codescanning

--- src/main/java/com/poc/security/models/User.java
+++ src/main/java/com/poc/security/models/User.java
@@ -10,13 +10,22 @@ public class User {
     private Long id;
     private String name;
     private String email;
-
+    private String password;
     public User() {}
 
-    public User(Long id, String name, String email) {
+    public User(Long id, String name, String email, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Long getId() {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (id, name, email) VALUES (1, 'John Doe', 'john@example.com');
-INSERT INTO users (id, name, email) VALUES (2, 'Jane Doe', 'jane@example.com');
+INSERT INTO users (id, name, email,password) VALUES (1, 'John Doe', 'john@example.com','1234');
+INSERT INTO users (id, name, email,password) VALUES (2, 'Jane Doe', 'jane@example.com','1234');
